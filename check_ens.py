833d48cd0134eff450126468ac1063c280db1ce6
Gaiseric <sebastiaancrisan@gmail.com>
2022-09-07 13:11:06 +0200

    get eth addr from ens and get balance

--- check_ens.py
+++ check_ens.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
 from web3 import Web3
+from ens import ENS
 
 load_dotenv()
 
 INFURA_API_KEY = os.getenv("INFURA_API_KEY")
 infura_url = f"https://mainnet.infura.io/v3/{INFURA_API_KEY}"
-web3 = Web3(Web3.HTTPProvider(infura_url))
-test_address = "elonmusk.eth"
+provider = Web3.HTTPProvider(infura_url)
+web3 = Web3(provider)
+ns = ENS(provider)
+ens_name = "elonmusk.eth"
 
-print("isConnected:", web3.isConnected())
-
-balance = web3.eth.getBalance(test_address)
-print("balance:", web3.fromWei(balance, "ether"))
+if(web3.isConnected()):
+    print("connected")
+    eth_address = ns.address(ens_name)
+    print("eth address:", eth_address)
+    balance = web3.eth.getBalance(eth_address)
+    print("balance:", web3.fromWei(balance, "ether"))
+else:
+    print("connection error")
 
